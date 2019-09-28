095ac679f749ab4b5436ab7de773bf57b8f389fe
goodvibesjumper <goodvibesjumper@hotmail.com>
2019-09-28 18:37:53 +0000

    wip

--- main.go
+++ main.go
@@ -8,51 +8,76 @@ import (
 
 func main() {
 	test := "1100110011001100000011000000111111001100111111001111110000000000000011001111110011111100111111000000110011001111110000001111110011001100000011"
-	fmt.Println(DiscoverTiming("1100110011001100000011000000111111001100111111001111110000000000000011001111110011111100111111000000110011001111110000001111110011001100000011"))
-	_ = getWordsFromMorseCode(test, 2)
+	//fmt.Println(GetTimingFromBitSentence("1100110011001100000011000000111111001100111111001111110000000000000011001111110011111100111111000000110011001111110000001111110011001100000011"))
+	_ = getWordsFromBitSentence(test)
+
+	//DiscoverTiming
+	//timing := DiscoverTimingFromBitSentence()
+	//ShrinkBitString
+	//GetWordsFromBitSentence
+	//GetLettersFromWords
+	//GetCharactersFromBitWord
 }
 
 
-func getWordsFromMorseCode(bitSequence string, timing int) []string {
-	ret := []string{}
-		
-	// first let's shrink
-	shrunk := ""	
-	for i, v := range bitSequence {
+func ShrinkBitSentence(bitSentence string, timing int) string {
+	ret := ""
+	for i, v := range bitSentence {
 		if (i % timing == 0) {
-			shrunk += string(v)
+			ret += string(v)
 		}
 	}
+	return ret
+}
 
-	fmt.Printf("Shrunk : %v\n", shrunk)
 
-	mcWords := strings.Split(shrunk, "000")
-	//fmt.Println(mcWords)
 
-	for _, mcWord := range mcWords {
-		fmt.Printf("mcWord = %v\n", mcWord)
+func getWordsFromBitSentence(bitSentence string) []string {
+	ret := []string{}
+		
+	timing := GetTimingFromBitSentence(bitSentence)
+	shrunk := ShrinkBitSentence(bitSentence, timing)		// some translators are slower than others
+	bitWords := strings.Split(shrunk, "0000000")				// break apart the words
+
+	for _, bitWord := range bitWords {
+		fmt.Printf("mcWord = %v\n", bitWord)
+		bitLetters := strings.Split(bitWord, "000")
+		for _, bitLetter := range bitLetters {
+			morseLetter := BitStringToMorseCodeString(bitLetter)
+			fmt.Println(morseLetter)
+			fmt.Println(string(MorseCodeLookupChar(morseLetter)))
+		}
+		
+		fmt.Println(bitLetters)
+		
+		/*
 		// this is in the form of a character string of 1's and 0's ie : 1010101 (which would be .... H)
-
+		tmp := BitStringToMorseCodeString(bitWord)
+		//ret = append(ret, MorseCodeLookupChar(tmp))
+		fmt.Println(MorseCodeLookupChar(tmp))*/
 	}
 
 	return ret
 }
 
-// BitStringToMorseCodeString will take a string in the form of "1010101" and convert to morse code "...."
+// BitStringToMorseCodeString will take a string in the form of "10101010" and convert to morse code "...."
 //  or 10111 to ".-" 
+// the provided bitSequence should already be shrunk
 func BitStringToMorseCodeString(bitSequence string) string {
-	//
-
+	tmp := strings.ReplaceAll(bitSequence, "1110","-")
+	tmp = strings.ReplaceAll(tmp, "10", ".")
+	tmp = strings.ReplaceAll(tmp, "1", ".")
+	return tmp
 }
 
-// DiscoverTiming will determine the number of characters that represents
+// DiscoverTimingFromBitSentence will determine the number of characters that represents
 // a single unit of time.
-func DiscoverTiming(bitSequence string) int {
+func GetTimingFromBitSentence(bitSentence string) int {
 	ret := 9999
 	lastChar := '0'
 	count := 0
 	
-	for _, v := range bitSequence {
+	for _, v := range bitSentence {
 		if (v=='1') {
 			count++
 			lastChar = '1'
--- main_test.go
+++ main_test.go
@@ -2,17 +2,95 @@ package main
 
 import (
 	"testing"
+	"fmt"
 )
 
-//MorseCodeLookupChar
 
-type TestCase struct {
-	seq	string
-	char byte
+func TestGetWordsFromBitSentence() {
+
 }
 
+func TestDiscoverTimingFromBitSentence(t *testing.T) {
+	type TestCase struct {
+		bitSentence string
+		expectedTiming int
+	}
+
+	testCases := []TestCase {
+		{
+			bitSentence: "1100110011001100000011000000111111001100111111001111110000000000000011001111110011111100111111000000110011001111110000001111110011001100000011", 
+			expectedTiming: 2,
+		},
+	}
+
+	for _, tc := range testCases {
+		got := GetTimingFromBitSentence(tc.bitSentence)
+		want := tc.expectedTiming
+
+		if got != want {
+			t.Errorf("bitSentence: %v\ngot: %v\nwant: %v", tc.bitSentence, got, want)
+		}
+	}
+}
+
+func TestShrinkBitSentence(t *testing.T) {
+	type TestCase struct {
+		bitSentence string
+		timing int
+		shrunkSentence string
+	}
+	
+	testCases := []TestCase {
+		{
+			bitSentence: "1100110011001100000011000000111111001100111111001111110000000000000011001111110011111100111111000000110011001111110000001111110011001100000011", 
+			timing: 2,
+			shrunkSentence: "10101010001000111010111011100000001011101110111000101011100011101010001",
+		},
+	}
+
+	for _, tc := range testCases {
+		got := ShrinkBitSentence(tc.bitSentence, tc.timing)
+		want := tc.shrunkSentence
+
+		if got != want {
+			t.Errorf("bitSentence: %v\ngot: %v\nwant: %v", tc.bitSentence, got, want)
+		}
+	}
+
+}
+
+func TestBitStringToMorseCodeString(t *testing.T) {
+	type TestCase struct {
+		onesAndZeros string
+		dotsAndDashes string
+	}
+
+	testCases := []TestCase {
+		{"10101010", "...."},
+		{"11101010", "-..."},
+	}
+
+	for _, tc := range testCases {
+		got := BitStringToMorseCodeString(tc.onesAndZeros)
+		want := tc.dotsAndDashes
+
+		if got != want {
+			t.Errorf("got: %v\nwant: %v", got, want)
+		}
+	}
+
+
+	got := BitStringToMorseCodeString("1010101")
+	fmt.Println(got)
+}
 
 func TestMorseCodeLookupChar(t *testing.T) {
+
+	type TestCase struct {
+		seq	string
+		char byte
+	}
+
 	testCases := []TestCase{
 		{".-", 'A'},
 		{"-...", 'B'},
